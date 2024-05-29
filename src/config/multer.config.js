1727df80260100db06504c3e46dafd3034b0d187
spykelionel <social.lionel@gmail.com>
2024-05-29 08:46:53 +0100

    update upload path

--- src/config/multer.config.js
+++ src/config/multer.config.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, `src/uploads/`);
+    cb(null, `./uploads/`);
   },
   filename: (req, file, cb) => {
     cb(
