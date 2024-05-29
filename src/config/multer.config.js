2debadcfda91ba4a5a76fef1c33aefb17875e519
spykelionel <social.lionel@gmail.com>
2024-05-29 08:44:02 +0100

    update file namings

--- src/config/multer.config.js
+++ src/config/multer.config.js
@@ -1,32 +1,31 @@
-const multer = require('multer');
+const multer = require("multer");
 // const path = require('node:path')
 // const fs = require('node:fs')
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, `./uploads/`);
-    },
-    filename: (req, file, cb) => {
-        // provide and invalid file type/extension to reduce file size to 0B on the server. 
-        // Because it will be 0Bytes
-        cb(
-            null,
-            `${(Date.now().toString()).replace("-", "")}_${file.originalname.replace(
-                " ",
-                "_"
-            )}`
-        );
-    },
+  destination: (req, file, cb) => {
+    cb(null, `src/uploads/`);
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${Date.now().toString().replace(/ /g, "-")}_${file.originalname.replace(
+        / /g,
+        "-"
+      )}`.replace(/-/g, "_")
+    );
+  },
 });
 
 const upload = multer({
-    storage,
-    fileFilter: (req, file, cb) => {
-        if (!file.mimetype.startsWith("image")) {
-            cb("File type not accepted. Must be an image");
-        }
-        cb(null, true);
-    },
+  storage,
+  fileFilter: (req, file, cb) => {
+    console.log(file);
+    if (!file.mimetype.startsWith("image")) {
+      cb("File type not accepted. Must be an image");
+    }
+    cb(null, true);
+  },
 });
 
 module.exports = upload;
